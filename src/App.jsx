28cd4fbdcf51e405f86d475699e682ee1c0c40ae
Muhammad Moaz <134685259+devbymoaz@users.noreply.github.com>
2024-09-04 17:17:59 +0500

    gfsdgzsg

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+import Landing from './page/Landing'
+
+function App() {
+
+  return (
+    <>
+
+      <Routes>
+        <Route path='/' element={<Landing />} />
+      </Routes>
+
+    </>
+  )
+}
+
+export default App
